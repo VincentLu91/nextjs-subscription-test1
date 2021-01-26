9a96cd7f2a53f4ffee3f7b52a25087b963d0ade8
Vince Lu <vincentlu@hotmail.ca>
2021-01-26 00:05:09 -0500

    blog page empty as CMS is not hooked up yet

--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,7 @@
 const blog = () => {
   return (
     <div>
-	  <h1>This is the empty blog page.</h1>
-	  <h2>Will hook up blog with CMS later.</h2>
+	  <h1>This will be updated</h1>
 	</div>
   );
 };
