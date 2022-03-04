e12a46b5cba4e9876eac5538a78bc201c98b6249
Vince Lu <vincentlu@hotmail.ca>
2022-03-03 21:35:30 -0500

    font styling (Menlo) of text on about page

--- pages/about.js
+++ pages/about.js
@@ -5,32 +5,40 @@ const about = () => {
     <div className={styles.container}>
       <Image src="/random.png" alt="" width={600} height={400} />
       <br />
-      <h2>Photorealistic content on demand</h2>
-      <p>
+      <h2 style={{ fontFamily: 'Menlo', fontSize: 30 }}>
+        Photorealistic content on demand
+      </h2>
+      <br />
+      <br />
+      <p style={{ fontFamily: 'Menlo' }}>
         Whether you are a solopreneur running an eCommerce store straight from
         your living room, or building a presence with a blog, or just trying to
         build an online business. We completely understand that it can be hard
         to stand out with visually aesthetic, on-brand content.
       </p>
-      <p>
+      <br />
+      <p style={{ fontFamily: 'Menlo' }}>
         In order to stand out from a sea of comptetitors, you will need
         compelling visual content to establish your credibility, and usually the
         images will need to be high quality to attract leads.
       </p>
-      <p>
+      <br />
+      <p style={{ fontFamily: 'Menlo' }}>
         An AI generated image system can help you achieve. We believe that this
         system will help you save time and money to curate high quality images
         on demand that will put you on par with established competitors so you
         could put them on your website, social media, blog, and other marketing
         materials.
       </p>
-      <p>
+      <br />
+      <p style={{ fontFamily: 'Menlo' }}>
         Our vision is to enable any ambitious entrepreneur to hit the ground
         running with efficient marketing processes no matter the amount of
         resources they have, so they could focus their time and energy on other
         value-producing functions, including product development and connecting
         with audiences.
       </p>
+      <br />
     </div>
   );
 };
