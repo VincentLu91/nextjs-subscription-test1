44f88409168427b08e925ad21cca56c985be65e2
Vince Lu <vincentlu@hotmail.ca>
2025-01-03 17:18:00 -0500

    January 3 2025: applied disabling of paywall in backend API calls

--- pages/api/modifyImage.js
+++ pages/api/modifyImage.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
 import { deductUserImageGenerationToken } from '../../utils/useDatabase';
 
-export default async function handler(req, res) {
+export default async function GET(req, res) {
   const { prompt, image, user } = req.query;
-  let result = await deductUserImageGenerationToken(user, 1);
-  if (result) {
-    // inpainting with input image and mask provided
+  try {
+    /*let result = await deductUserImageGenerationToken(user, 1);
+
+    if (!result)
+      return res.status(403).json({
+        error: 'User doesnothave permission'
+      });*/
+
     const resp = await axios.post(
       // send prediction to dashboard but it takes time to generate image
       'https://api.replicate.com/v1/predictions',
@@ -35,9 +40,11 @@ export default async function handler(req, res) {
       }
     );
     res.json(resp.data.urls);
-  } else {
-    res.status(403).json({
-      error: 'User doesnothave permission'
+  } catch (error) {
+    const errorMessage = error?.response?.data?.message || error?.message;
+
+    res.status(400).json({
+      error: errorMessage
     });
   }
 }
