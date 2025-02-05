e7bbe40db4389acc7c697166128aeb62b63f5258
Vince Lu <vincentlu@hotmail.ca>
2025-02-04 20:10:39 -0500

    February 4 2025: integrated posthog

--- pages/_app.js
+++ pages/_app.js
@@ -4,19 +4,48 @@ import '../assets/chrome-bug.css';
 import { useEffect } from 'react';
 import Layout from '../components/Layout';
 import { UserContextProvider } from '../components/UserContext';
+import { Router } from 'next/router';
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({
+  Component,
+  pageProps: { session, ...pageProps }
+}) {
   useEffect(() => {
     document.body.classList?.remove('loading');
   }, []);
 
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+      api_host:
+        process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+      // Enable debug mode in development
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === 'development') posthog.debug();
+      }
+    });
+
+    const handleRouteChange = () => posthog?.capture('$pageview');
+
+    Router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, []);
+
   return (
-    <div className="bg-primary">
-      <UserContextProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </UserContextProvider>
-    </div>
+    <>
+      <PostHogProvider client={posthog}>
+        <div className="bg-primary">
+          <UserContextProvider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </UserContextProvider>
+        </div>
+      </PostHogProvider>
+    </>
   );
 }
