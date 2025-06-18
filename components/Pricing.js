11d6cbcaf63ea8b4180cfe5ef66deef32c1931b5
Vince Lu <vincentlu@hotmail.ca>
2025-06-18 12:18:58 -0400

    June 18 2025: filtered out lifetime $0 plan from rendering

--- components/Pricing.js
+++ components/Pricing.js
@@ -102,7 +102,9 @@ export default function Pricing({ products }) {
         <div className="mt-12 space-y-4 sm:mt-16 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-4">
           {products.map((product) => {
             const price = product.prices.find(
-              (price) => price.interval === billingInterval
+              (price) =>
+                price.interval === billingInterval &&
+                price.product_id !== 'prod_SW7dE3i2msnicA' // $0 subscription plan for early users
             );
             const priceString = new Intl.NumberFormat('en-US', {
               style: 'currency',
