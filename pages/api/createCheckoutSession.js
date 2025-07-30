819f1d06041984412adb18399f2c2f756c09953a
Vince Lu <vincentlu@hotmail.ca>
2025-07-30 11:33:29 -0400

    testing stripe change #1

--- pages/api/createCheckoutSession.js
+++ pages/api/createCheckoutSession.js
@@ -46,13 +46,15 @@ const createCheckoutSession = async (req, res) => {
             end_behavior: {
               missing_payment_method: 'pause'
             }
-          },
-          trial_period_days: 30
+          }
+          // trial_period_days: 30
         },
-        payment_method_collection: 'if_required',
+        // payment_method_collection: 'if_required',
+        payment_method_collection: 'always',
         success_url: `${getURL()}/account`,
         cancel_url: `${getURL()}/`
       });
+      console.log('session57==============', session);
 
       return res.status(200).json({ sessionId: session.id, url: session.url });
     } catch (err) {
